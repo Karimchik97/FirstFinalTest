aec375593a0663fe1a36145a73de746f978aa734
Diana <Ladidi997@mail.ru>
2022-09-19 21:44:34 +0300

    First condition to solve the task

--- Program.cs
+++ Program.cs
@@ -6,3 +6,15 @@ Console.Clear();
 
 string[] FirstArray = new string[5] {"123", "23", "hello", "world", "res"};
 string[] SecondArray = new string[FirstArray.Length];
+
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(FirstArray[i].Length <= 3)
+        {
+        SecondArray[count] = FirstArray[i];
+        count++;
+        }
+    }
+}
