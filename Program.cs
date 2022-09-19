77b85a149b78d7ab54a4020c2e474b801a9a7fb5
Diana <Ladidi997@mail.ru>
2022-09-19 21:43:15 +0300

    Array initialisation

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿/*Необходимо написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. 
+*/ 
+
+Console.Clear();
+
+string[] FirstArray = new string[5] {"123", "23", "hello", "world", "res"};
+string[] SecondArray = new string[FirstArray.Length];
